91d7bdd21393bee697936df4001744a3ba5ef18e
Ruth Balbina Figueredo <ruth_lestat@hotmail.com>
2023-09-26 16:42:56 -0300

    finished homework class 3

---
+++ index.js
@@ -0,0 +1,61 @@
+//EJERCICIO 1
+//Crear una funcion que reciba como argumento un string y devuelva la cantidad de caracteres que tenga el mismo.
+
+// const cantidadDeCaracteres = prompt("Escribe tu frase favorita:")
+// console.log(cantidadDeCaracteres.length)
+
+//EJERCICIO 2
+//Tomar la siguiente variable, convertirla a todo en mayuscula y mostrar por consola. Hacer lo mismo pero con minuscula.
+
+// let string = "El artista WOS cantando QUIEREME"
+
+// console.log(string.toUpperCase())
+
+// console.log(string.toLowerCase())
+
+//EJERCICIO 3
+
+//Tomar la variable del ejercicio 2 y cortar el string devolviendo solo el WOS y hacer el mismo proceso para devolver QUIEREME
+
+// let string = "El artista WOS cantando QUIEREME"
+
+// console.log(string.slice(11, 15))
+// console.log(string.slice(24))
+
+//EJERCICIO 4
+//Tomar la variable del ejercicio 2 y reemplazar WOS por QUEEN. Y QUIEREME por Bohemian Rapsody
+
+// let string = "El artista de WOS cantando QUIEREME"
+
+// console.log(string.replace("WOS", "QUEEN"))
+// console.log(string.replace("QUIEREME", "Bohemian Rapsody"))
+
+//EJERCICIO 5
+//Crear una funcion que tome por parámetro un numero de DNI y retorne el mismo valor como un string
+
+// const indicaDNI = prompt("Escribe tu numero de documento:")
+
+// console.log(indicaDNI.toString())
+// console.log(typeof indicaDNI.toString())
+
+//EJERCICIO 6
+//Crear una funcion que tome por parametro al menos 4 numeros y retorne el numero menor de ellos
+
+// console.log(Math.min(45, 18, -1, 120))
+
+//EJERCICIO 7
+//Crea una funcion que tome por argumento un numero y retorne un numero aleatorio entre 0 y el argumento
+
+// console.log(Math.random(55))
+
+//EJERCICIO 8
+//Tomar la siguiente variable y lograr las siguientes tareas:
+//-Redondear el numero hacia arriba
+//-Redondear el numero hacia abajo
+
+// let number = 12.49
+
+// console.log(Math.ceil(number))
+// console.log(Math.floor(number))
+
+
